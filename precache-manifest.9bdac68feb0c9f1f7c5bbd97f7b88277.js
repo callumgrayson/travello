a2c9b1d6b6f4011d94abff9a38a1991a2d879d4f
callumgrayson <callum@musiio.com>
2019-09-05 16:34:56 +0800

    Updates

---
+++ precache-manifest.9bdac68feb0c9f1f7c5bbd97f7b88277.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220417d47209c41a7c2ac28612a15bca",
+    "url": "/travello/index.html"
+  },
+  {
+    "revision": "613634f8f68eac4c8aa8",
+    "url": "/travello/static/css/main.929fb8cd.chunk.css"
+  },
+  {
+    "revision": "de986259ec36230964bf",
+    "url": "/travello/static/js/2.e5d629f5.chunk.js"
+  },
+  {
+    "revision": "613634f8f68eac4c8aa8",
+    "url": "/travello/static/js/main.c910662e.chunk.js"
+  },
+  {
+    "revision": "cf8cbb9bdc90bc84cc3d",
+    "url": "/travello/static/js/runtime~main.d973a9af.js"
+  },
+  {
+    "revision": "dff8f8c9c67c290ff58132e8c832cd8b",
+    "url": "/travello/static/media/chevron-thin-right.dff8f8c9.svg"
+  },
+  {
+    "revision": "e7ae3e52025585dfbcbe1ae8daf42540",
+    "url": "/travello/static/media/hotel-1.e7ae3e52.jpg"
+  },
+  {
+    "revision": "0ef736b7e3c28d3392337f4d2a2dcb87",
+    "url": "/travello/static/media/hotel-2.0ef736b7.jpg"
+  },
+  {
+    "revision": "54ceec13e70120056604f93bd7e07e41",
+    "url": "/travello/static/media/hotel-3.54ceec13.jpg"
+  },
+  {
+    "revision": "6745dd393dbf682b1746e34480216f60",
+    "url": "/travello/static/media/sprite.6745dd39.svg"
+  }
+]);
